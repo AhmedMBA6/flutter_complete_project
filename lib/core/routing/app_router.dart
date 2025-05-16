b0368a5ee240fa3326390d6abd1dfccb9f3a395f
AhmedMBA6 <https://github.com/AhmedMBA6>
2025-05-16 14:13:03 +0300

    system design and route the app to on boarding screen

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_project/features/login/ui/login_screen.dart';
+import 'package:flutter_complete_project/features/onboarding/onboarding_screen.dart';
+
+import 'routes.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(
+          builder: (context) => const OnboardingScreen(),
+        );
+      case Routes.loginScreen:
+        return MaterialPageRoute(
+          builder: (context) => const LoginScreen(),
+        );
+
+      default:
+        return MaterialPageRoute(
+          builder: (context) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
