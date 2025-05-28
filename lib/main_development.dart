b4ee8d717995864f869ae5df0c75f56b55ed8bbe
AhmedMBA6 <https://github.com/AhmedMBA6>
2025-05-28 07:15:05 +0300

    - set up Flutter flavors for Android (Development & Production flavors) - changed android app name - set up launch configurations - added launch.json - added main file for each flavor

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_project/core/di/dependency_injection.dart';
 import 'package:flutter_complete_project/core/routing/app_router.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'doc_app.dart';
 
-void main() {
+void main() async{
   setupGetIt();
+  // Ensure that the screen size is initialized before running the app as release mode.
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
